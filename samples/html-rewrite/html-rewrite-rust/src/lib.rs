d86b177f5ffb76e343d7b56485b81f1bd35b9b77
Thorsten Hans <thorsten.hans@fermyon.com>
2025-07-29 12:17:16 +0200

    chore: Add ttl to HTML rewrite Rust sample
    Signed-off-by: Thorsten Hans <thorsten.hans@fermyon.com>

--- samples/html-rewrite/html-rewrite-rust/src/lib.rs
+++ samples/html-rewrite/html-rewrite-rust/src/lib.rs
@@ -1,30 +1,39 @@
 use lol_html::{element, rewrite_str, RewriteStrSettings};
 use spin_sdk::http::{send, IntoResponse, Request, RequestBuilder, Response, ResponseBuilder};
+use spin_sdk::http_component;
 use spin_sdk::key_value::Store;
-use spin_sdk::{http_component, variables};
+
+use crate::cache::CacheItem;
+use crate::config::Config;
+
+mod cache;
+mod config;
 
 #[http_component]
 async fn handle_html_rewrite(req: Request) -> anyhow::Result<impl IntoResponse> {
-    let upstream_url = variables::get("upstream_url").expect("Upstream URL not configured");
-    let path = req
-        .path_and_query()
-        .filter(|&s| !s.is_empty() && s != "/")
-        .unwrap_or("/index.html");
-    let url = format!("{upstream_url}{path}");
+    let Ok(cfg) = Config::load() else {
+        println!("Application Configuration missing or invalid");
+        return Ok(Response::new(500, ()));
+    };
 
-    let use_kv_store = variables::get("use_kv_store")
-        .and_then(|v| Ok(v == "true"))
-        .unwrap_or_default();
-    if (use_kv_store) {
+    let url = build_upstream_url(&req, cfg.upstream_url.clone());
+
+    if cfg.use_key_value_store {
         let kv = Store::open_default()?;
-        let found = kv.get(url.as_str())?;
+        let key = url.as_str();
+        let found = CacheItem::load_by_key(&kv, key)?;
         if found.is_some() {
-            println!("Cache hit");
-            let original = String::from_utf8_lossy(&found.unwrap()).to_string();
-            return Ok(ResponseBuilder::new(200)
-                .header("content-type", "text/html")
-                .body(rewrite_html(original))
-                .build());
+            let found = found.unwrap();
+            if found.is_valid(cfg.ttl_in_minutes) {
+                println!("Cache hit: Item is still valid");
+                return Ok(ResponseBuilder::new(200)
+                    .header("content-type", "text/html")
+                    .body(rewrite_html(found.value))
+                    .build());
+            } else {
+                println!("Cache hit: Item is expired");
+                CacheItem::delete_by_key(&kv, key)?;
+            }
         }
     }
 
@@ -46,9 +55,10 @@ async fn handle_html_rewrite(req: Request) -> anyhow::Result<impl IntoResponse> 
     }
     let input = String::from_utf8_lossy(response.body()).to_string();
 
-    if use_kv_store {
+    if cfg.use_key_value_store {
         let kv = Store::open_default()?;
-        kv.set(&url, input.as_bytes())?;
+        let item = CacheItem::new(input.clone());
+        item.store_at_key(&kv, &url)?;
     }
     Ok(ResponseBuilder::new(200)
         .header("content-type", "text/html")
@@ -73,4 +83,12 @@ fn rewrite_html(input: String) -> String {
         },
     )
     .unwrap_or(input);
+}
+
+fn build_upstream_url(req: &Request, upstream_base_url: String) -> String {
+    let path = req
+        .path_and_query()
+        .filter(|&s| !s.is_empty() && s != "/")
+        .unwrap_or("/index.html");
+    format!("{}{}", upstream_base_url, path)
 }
